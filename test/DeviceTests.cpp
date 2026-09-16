8b3e0b56b3621ae9ce523d5fa06d64bd9ee09616
juuxi <dimashopyrev@mail.ru>
2026-09-16 19:08:44 +0300

    add 2 devices receive ids + device receives data tests

--- test/DeviceTests.cpp
+++ test/DeviceTests.cpp
@@ -32,3 +32,53 @@ TEST(TemperatureDeviceTest, ReceiveId) {
     temperatureDevice.receiveId();
     EXPECT_EQ(temperatureDevice.getId(), 1);
 }
+
+TEST(DevicesTest, ReceiveId) {
+    auto mockServer = std::make_shared<MockDeviceServer>();
+    LightDevice lightDevice(mockServer);
+    TemperatureDevice temperatureDevice(mockServer);
+
+    EXPECT_CALL(*mockServer, giveDeviceId(DeviceType::LIGHT_DEVICE))
+        .Times(1)
+        .WillOnce(::testing::Return(1));
+
+    EXPECT_CALL(*mockServer, giveDeviceId(DeviceType::TEMPERATURE_DEVICE))
+        .Times(1)
+        .WillOnce(::testing::Return(2));
+
+    lightDevice.receiveId();
+    EXPECT_EQ(lightDevice.getId(), 1);
+
+    temperatureDevice.receiveId();
+    EXPECT_EQ(temperatureDevice.getId(), 2);
+}
+
+TEST(LightDeviceTest, ReceiveData) {
+    auto mockServer = std::make_shared<MockDeviceServer>();
+    LightDevice lightDevice(mockServer);
+
+    EXPECT_CALL(*mockServer, giveDeviceId(DeviceType::LIGHT_DEVICE))
+        .Times(1)
+        .WillOnce(::testing::Return(1));
+
+    EXPECT_CALL(*mockServer, receiveData(1, DeviceData("full")))
+        .Times(1);
+
+    lightDevice.receiveId();
+    lightDevice.receiveData("full");
+}
+
+TEST(TemperatureDeviceTest, ReceiveData) {
+    auto mockServer = std::make_shared<MockDeviceServer>();
+    TemperatureDevice temperatureDevice(mockServer);
+
+    EXPECT_CALL(*mockServer, giveDeviceId(DeviceType::TEMPERATURE_DEVICE))
+        .Times(1)
+        .WillOnce(::testing::Return(1));
+
+    EXPECT_CALL(*mockServer, receiveData(1, DeviceData(25)))
+        .Times(1);
+
+    temperatureDevice.receiveId();
+    temperatureDevice.receiveData(25);
+}
